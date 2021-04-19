17ea6306c0be8e645c7a8b66a84ec447ac060903
Mersad Ajanovic <34665379+flamerged@users.noreply.github.com>
2021-04-19 10:09:58 +0200

    added new practice from udemy course

---
+++ basic assignment 1/app.js
@@ -0,0 +1,12 @@
+const basicAssignment = Vue.createApp({
+  data() {
+    return {
+      name: "Mersad",
+      age: 31,
+      image:
+        "https://images.unsplash.com/photo-1510364966834-1c2f9fd921d7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1301&q=80",
+    };
+  },
+});
+
+basicAssignment.mount("#assignment");
